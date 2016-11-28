f0450d545eafc780091190ebe0727687167d6487
clkindle <clkindle@live.com>
2016-11-28 09:49:16 -0800

    ex2 commit

---
+++ test1.py
@@ -0,0 +1,8 @@
+vendor = 'Cisco'
+model = 'Nexus'
+serial_number = 'NXOS123'
+user = raw_input("Enter username: ")
+
+print
+print '{:<30} {:<30} {:<30} {:<30}'.format(vendor, model, serial_number, user)
+print
